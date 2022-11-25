794529f90f9388f367e996758a146bc410905c26
stefu <sotajagic@gmail.com>
2022-11-25 18:55:05 +0200

    refactored analyzeData a little

--- simulation/analyzeData.py
+++ simulation/analyzeData.py
@@ -5,11 +5,24 @@ import sys
 import csv
 
 
-def get_data(filename):
+def read_file(filename):
     data = None
     with open(filename, "r") as f:
         csv_data = csv.DictReader(f, delimiter="\t")
-        data = list(map(lambda row: {key: float(row[key]) for key in row }, csv_data))
+        data = list(map(lambda row: {key: float(row[key]) for key in row}, csv_data))
+    return data
+
+
+def get_data():
+    if len(sys.argv) < 2:
+        raise ValueError("Provide a filename or two filenames as arguments")
+    data = read_file(sys.argv[1])
+    if len(sys.argv) > 2:
+        print("Two files detected. Performing analysis for their differences")
+        more_data = read_file(sys.argv[2])
+        if len(more_data) != len(data):
+            raise ValueError("Both files should have the same amount of data")
+        data = [{key: a[key] - b[key] for key in a} for a, b in zip(data, more_data)]
     return data
 
 
@@ -19,27 +32,17 @@ def get_mean_and_ci(data, value_name) -> Tuple[float, Tuple[float, float]]:
     ci = conf_interval(1.96, len(values), mean, np.std(values))
     return (mean, ci)
 
+
 def print_value_info(data, value_name):
-    (mean, ci) = get_mean_and_ci(
-        data, value_name
-    )
+    (mean, ci) = get_mean_and_ci(data, value_name)
     print(f"Data for {value_name}")
     print(f"Mean = {mean}")
     print(f"Confidence interval = {ci}")
 
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        raise ValueError("Provide a filename or two filenames as arguments")
-    data = get_data(sys.argv[1])
-    if len(sys.argv) > 2:
-        print("Two files detected. Performing analysis for their differences")
-        more_data = get_data(sys.argv[2])
-        if len(more_data) != len(data):
-            raise ValueError("Both files should have the same amount of data")
-        data = [{key: a[key] - b[key] for key in a} for a, b in zip(data, more_data)]
-    
+    data = get_data()
     interesting_values = ["wait_time", "entrance_queue", "operation_time"]
     for value in interesting_values:
         print_value_info(data, value)
-        print("*"*80)
-
+        print("*" * 80)
